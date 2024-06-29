0e2cb44e6d4859257e826a43d97b7f652cc0dbb9
kenkomu <komukenneth216@gmail.com>
2024-06-29 10:21:20 +0300

    completed the move semantics quiz

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 #[test]
 fn main() {
@@ -14,8 +14,8 @@ fn main() {
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+fn fill_vec(vec0: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec0;
 
     vec.push(88);
 
--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -5,22 +5,30 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
+    let vec3 = vec![22, 44, 66, 88];
 
-    let mut vec1 = fill_vec(vec0);
+
+    
+
+    let  vec4= fill_vec(vec3.clone());
 
     assert_eq!(vec0, vec![22, 44, 66]);
-    assert_eq!(vec1, vec![22, 44, 66, 88]);
+    
+    assert_eq!(vec4, vec![22, 44, 66, 88, 88]);
+
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec3: Vec<i32>) ->  Vec<i32> {
+    
+    let mut vecc = vec3;
 
-    vec.push(88);
+    
+    vecc.push(88);
 
-    vec
+    vecc
 }
+
